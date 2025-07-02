bc51b928c47e9c60630327e7acae7e7bdbe9ab08
Rohith.B <rohithbrock9164@gmail.com>
2025-07-02 10:44:22 +0530

    refactor(reversepattern): change printed value from asterisk to number
    Modify the inner loop to print the current iteration number (j) instead of asterisks to better visualize the pattern structure

--- Python/reversepattern.py
+++ Python/reversepattern.py
@@ -6,7 +6,8 @@ while i <= 5:
         b = b + 1
     j = 1
     while j <= i:
-        print("*", end = "")
+        # print(i, end = "")
+        print(j, end = "")
         j = j + 1
     print()
     i = i + 1
